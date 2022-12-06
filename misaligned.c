249d4f76a50693438b5bc08727d5f7044eed7dff
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-12-06 20:39:46 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -18,6 +18,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            testColorPair(i,j,majorColor[i],minorColor[i]);
         }
     }
     return i * j;
