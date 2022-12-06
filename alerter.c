02a131e2c27fbfc816175a189a42298d17580d35
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-12-06 20:55:56 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -26,6 +26,7 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+    void (celcius);
 }
 
 int main() {
